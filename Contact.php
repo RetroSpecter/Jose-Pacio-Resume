737707d1abc60578d39ee8dfc2f2028875c10177
RetroSpecter <InsertNameHere4897@gmail.com>
2017-02-02 14:38:22 -0800

    updates

---
+++ Contact.php
@@ -0,0 +1,59 @@
+<?php 
+	include("Common.php");
+	top();
+?>
+		<div id="content">
+			<br /> <?-- ugly code fix this later --?>
+			<br />
+			<br />
+			<h1> Contact Me </h1>
+<!--
+
+			<form class="row" action="https://docs.google.com/forms/d/e/1FAIpQLScYAGvpbPOVubSaf_owjQfxsxAsYrBuQDO-fiybiy3IWgY8uA/formResponse" method="POST" target="_self">
+				<div class="col-half">
+					<label class="hidden" aria-label="(Required field)">
+						Name
+					</label>
+					<input type="text" name="entry.2005620554" value="" aria-label="Name" aria-required="true" required="" placeholder="Name">
+					<label class="hidden" aria-label="(Required field)">
+						Email
+					</label>
+					<input type="email" name="entry.1045781291" value="" aria-label="Email" aria-required="true" required="" title="Must be a valid email address" placeholder="Email">
+				</div>
+				<div class="col-half">
+					<label class="hidden" aria-label="(Required field)">
+						Message
+					</label>
+					<textarea name="entry.839337160" aria-label="Message" aria-required="true" required="" placeholder="Message"></textarea>
+				</div>
+				<div class="col-full">
+					<button class="button" type="submit" name="submit">Send</button>
+				</div>
+			</form>
+-->
+		<br />
+		<br />
+		<br />
+		Phone: +1-253-227-8337
+		<br />
+		<br />
+		<br />
+		Email: <a href =""> josepacio@comcast.net </a>
+		<br />
+		<br />
+		<br />
+		LinkedIn: <a href="https://www.linkedin.com/in/jose-pacio-b6254599 "> https://www.linkedin.com/in/jose-pacio-b6254599 </a>
+		<br />
+		<br />
+		<br />
+		Twitter: <a href="https://twitter.com/ImPACIOble"> @ImPACIOble </a>
+		<br />
+		<br />
+		<br />
+		
+		</div>
+		<div id="footer">
+			Created by Jose Pacio 2017
+		</div>
+	</body>
+</html>
---
+++ Resume.php
@@ -0,0 +1,57 @@
+<?php 
+	include("Common.php");
+	top();
+?>
+			<div class="subsection">
+				<h2>Experience</h2>
+				<p>
+					<span class="subtitle">Bulldozer Games</span>, Seattle, Washington <span class="right">2016 (4 months)</span>
+					<br />
+					Level Programmer and Designer, Smashy Brick (mobile)
+					<br />
+					<ul>
+						<li>Level design for World 4</li>
+						<li>Code Debugging and Optimization</li>
+						<li>Level design tools</li>
+					</ul>
+					
+				</p>
+				<p>
+					<span class="subtitle">University of Washington Game Development Club</span>, Seattle, Washington <span class="right">2015-present</span>
+					<br />
+					Vice-President (2016-present)
+					<br />
+					<ul>
+						<li>Lead tutorials for game development software such as Unity</li>
+						<li>Foster the Game Development community around the University of Washington</li>
+						<li>Organize and coordinate club meetings and events</li>
+					</ul>
+					
+				</p>
+				<p>
+					<span class="subtitle">Tacoma Metro Parks</span>, Tacoma, Washington <span class="right">2016 (1 month)</span>
+					<br />
+					Video Production Summer Camp Instructor
+					<br />
+					<ul>
+						<li>Designed entire curriculum for video production</li>
+						<li>Mentored middle school students to create their video projects</li>
+					</ul>
+					
+				</p>
+				For <span class="subtitle">personal projects</span>, visit link
+			</div>
+			<div class="subsection">
+				<h2>Education</h2>
+				<span class="subtitle">University of Washington</span>, Seattle, WA <span class="right">2015 - present</span>
+				<br />
+				intended Computer Science major 
+			</div>
+		<hr />
+		Download PDF | DOCX
+		</div>
+		<div id="footer">
+			Created by Jose Pacio 2017
+		</div>
+	</body>
+</html>
